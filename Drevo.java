ea39072108b2daa8653a8028c963a8b45632dda3
PrusovAB <Djromeo93@mail.ru>
2023-01-03 20:07:33 +0300

    dd

---
+++ Drevo.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Drevo {
+
+   public static void addHuman(Human[] human) { // добавление
+      List<Human[]> humans = new ArrayList<>();
+      for (int i = 0; i < humans.size(); i++) {
+         humans.addAll(Collections.singleton(human));
+      }
+
+   }
+}
